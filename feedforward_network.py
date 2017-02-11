d5dc3c61c55233df61cbe75481d43b8b553e3acf
G Zweigle <gzweigle@users.noreply.github.com>
2017-02-10 19:42:33 -0800

    added feedforward network

---
+++ feedforward_network.py
@@ -0,0 +1,27 @@
+# Feedforward network
+#
+# To Do: Remove hardcoding of the depth.
+#
+
+# External modules.
+import numpy as np
+
+# Modules specific to this program.
+import nonlinearities as nl
+
+
+def feedforward_network(inval, A2, b2, A3, b3, A4, b4):
+
+    z2 = np.dot(A2,inval)
+    z2 = np.add(z2, b2)
+    z2 = nl.nonlinearity(z2)
+    
+    z3 = np.dot(A3,z2)
+    z3 = np.add(z3, b3)
+    z3 = nl.nonlinearity(z3)
+    
+    z4 = np.dot(A4,z3)
+    z4 = np.add(z4, b4)
+    z4 = nl.sigmoid(z4)
+    
+    return z2, z3, z4
